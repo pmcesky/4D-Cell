2315aa04c34ea09bf8c827d9d58d52b06f678a1f
pmcesky <moptmpgq@gmail.com>
2023-10-26 23:08:48 -0500

    method to extract data for model training and test
    Write a function to extract and construct the data for model training and testing

--- _utilities.py
+++ _utilities.py
@@ -12,6 +12,72 @@ from collections import deque, defaultdict
 
 
 
+def prepare_data_for_model(embryo_cells_info, embryo_samples, use_frame = True, lifespan_frame_longest = 50, preserve_time_dimension = True, flatten = False):
+    """
+    Prepare the data for model training and testing. The function will extract data from embryo_cells_info based on embryo_samples.
+
+    Parameters
+    ----------
+    embryo_cells_info : a dict of dict
+        a dict of dict. Outer dict key is embryo_name, inner dict key is cell name, values are cell_features in dict.
+    embryo_samples : a list of embryo names.
+        a list of inquired embryo names, this will be used to extract the data from embryo_cells_info corresponding to inquired embryos.
+    use_frame : bool, optional
+        either use frame or use time version in trajectory, start_time and lifespan, by default True. 
+        If use_frame, then related features will be trajectory_processed, start_frame, and lifespan_frame;
+        If not use_frame, then related features will be trajectory_processed_txyz, start_time, and lifespan_time; 
+    lifespan_frames_longest : int, optional
+        will append the trajectory at the end if the trajectory is shorter than this number, by default 50.
+    preserve_time_dimension : bool, optional
+        either preserve time/frame dimension in trajectory, by default True. 
+        If true, the trajectory (frame, x, y, z) or (t, x, y, z) will be preserved, otherwise only (x, y, z) will show up.
+    flatten: bool, optional
+        either flatten cell_features, by default False
+        if False, cell_features will be a list of list, each element in inner list will be numpy array or scaler. 
+        If True, cell features will be a list of 1D array, all elements in original list will be flatten and append in order.
+
+    Return
+    ----------
+    cell_features: a list of list
+        the inner list is a list of cell features of one cell, ordered in [trajectory, start_time, lifespan, division_orientation_to_mother_cells, division_orientation_to_daughter_cells].
+    cell_names: a list of cell names.
+        same order as cell_features. 
+    """ 
+    cell_names = []
+    cell_features = []
+    assert set(embryo_cells_info.keys()) >= set(embryo_samples)
+    for embryo_name in embryo_samples:
+        for cell in embryo_cells_info[embryo_name]:
+            cell_names.append(cell)
+            current_cell_info =  embryo_cells_info[embryo_name][cell]
+            if use_frame:
+                features = [current_cell_info['trajectory_processed'].to_numpy(),\
+                    current_cell_info['start_frame'],current_cell_info['lifespan_frame'],\
+                    current_cell_info['division_orientation_to_mother_cell'],current_cell_info['division_orientation_of_daughter_cells']]
+            else:
+                features = [current_cell_info['trajectory_processed_txyz'].to_numpy(),\
+                    current_cell_info['start_time'],current_cell_info['lifespan_time'],\
+                    current_cell_info['division_orientation_to_mother_cell'],current_cell_info['division_orientation_of_daughter_cells']]
+            # append trajectory lifespan to lifespan_frame_longest
+            if current_cell_info['trajectory_processed'].shape[0]<lifespan_frame_longest:
+                # append -1000 at the end
+                padded = -1000*np.ones((lifespan_frame_longest,4)).astype(float)
+                padded[:features[0].shape[0],:] = features[0] 
+                features[0] = padded.copy()
+            if not preserve_time_dimension: # discard frame/time dimension
+                features[0] = features[0][:,1:] # (x,y,z)
+            if flatten:
+                features_flattened = features[0].flatten()
+                features_flattened = np.append(features_flattened, [features[1], features[2]])
+                features_flattened = np.append(features_flattened, features[3].flatten())
+                features_flattened = np.append(features_flattened, features[4].flatten())
+                features_flattened = features_flattened.astype(float)
+                features = features_flattened.copy()
+            cell_features.append(features.copy())
+    return cell_features, cell_names
+
+
+
 def plot_cell_trajectory(cells_info, cell, figsize = (10,10), azim=225, save_path = None):
     fig = plt.figure(figsize = figsize)
     ax = plt.axes(projection='3d')
