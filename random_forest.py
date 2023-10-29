e7e98483f350614697ab7e8b51adaf1c8ac8c111
pmcesky <moptmpgq@gmail.com>
2023-10-28 22:51:35 -0500

    Random Forest feature ablation cross-validation
    Cross-validation:
    1. only trajectory features
    2. trajectory + start_frame
    3. trajectory + start_frame + lifespan_frame
    4. trajectory + start_frame + lifespan_frame + division_orientation_to_mother_cell
    5. all features

--- random_forest.py
+++ random_forest.py
@@ -28,6 +28,7 @@ embryos_for_test = ['WT-EMB05','WT-EMB12','WT-EMB19','WT-EMB26']
 embryos_for_cross_validation = [embryo_name for embryo_name in embryo_cells_info if embryo_name not in embryos_for_test]
 
 
+########################################## Random Forest use all features ###########################################################################################
 # changing n_estimators in the forest
 rf_cv_score = []
 for i in tqdm(range(6)): # 6-fold cross-validation
@@ -40,11 +41,116 @@ for i in tqdm(range(6)): # 6-fold cross-validation
     # X_val, y_val = shuffle(X_val, y_val, random_state=1)
     scores = []
     for n_trees in range(10, 201):
-        forest = RandomForestClassifier(n_estimators=n_trees, random_state=1, n_jobs=100)
+        forest = RandomForestClassifier(n_estimators=n_trees, random_state=1, n_jobs=10)
+        forest.fit(X_train, y_train)
+        scores.append(forest.score(X_val, y_val))
+    rf_cv_score.append(scores.copy())
+
+with open( './rf_cv_score_all_features.json', 'w') as f:
+   json.dump(rf_cv_score, f)
+
+
+
+########################################## Random Forest use only trajectory ###########################################################################################
+# changing n_estimators in the forest
+rf_cv_score = []
+for i in tqdm(range(6)): # 6-fold cross-validation
+    # train and val dataset
+    embryos_val = embryos_for_cross_validation[4*i:4*i+4]
+    embryos_train = [embryo_name for embryo_name in embryos_for_cross_validation if embryo_name not in embryos_val]
+    X_train, _, y_train = _utilities.prepare_data_for_model(embryo_cells_info, embryos_train, use_frame = True, lifespan_frame_longest = 50, preserve_time_dimension = True, flatten = True)
+    X_val, _, y_val = _utilities.prepare_data_for_model(embryo_cells_info, embryos_val, use_frame = True, lifespan_frame_longest = 50, preserve_time_dimension = True, flatten = True)
+    # use only trajectory features
+    X_train = np.array(X_train)[:,:200] 
+    X_val = np.array(X_val)[:,:200]
+    X_train, y_train = shuffle(X_train, y_train, random_state=1)
+    # X_val, y_val = shuffle(X_val, y_val, random_state=1)
+    scores = []
+    for n_trees in range(10, 201):
+        forest = RandomForestClassifier(n_estimators=n_trees, random_state=1, n_jobs=10)
         forest.fit(X_train, y_train)
         scores.append(forest.score(X_val, y_val))
     rf_cv_score.append(scores.copy())
 
 
-with open( './rf_cv_score_all_features.json', 'w') as f:
-   json.dump(rf_cv_score, f)
+with open( './rf_cv_score_trajectory.json', 'w') as f:
+    json.dump(rf_cv_score, f)
+
+
+
+########################################## Random Forest use only trajectory + start_frame ###########################################################################################
+# changing n_estimators in the forest
+rf_cv_score = []
+for i in tqdm(range(6)): # 6-fold cross-validation
+    # train and val dataset
+    embryos_val = embryos_for_cross_validation[4*i:4*i+4]
+    embryos_train = [embryo_name for embryo_name in embryos_for_cross_validation if embryo_name not in embryos_val]
+    X_train, _, y_train = _utilities.prepare_data_for_model(embryo_cells_info, embryos_train, use_frame = True, lifespan_frame_longest = 50, preserve_time_dimension = True, flatten = True)
+    X_val, _, y_val = _utilities.prepare_data_for_model(embryo_cells_info, embryos_val, use_frame = True, lifespan_frame_longest = 50, preserve_time_dimension = True, flatten = True)
+    # use only trajectory features
+    X_train = np.array(X_train)[:,:201] 
+    X_val = np.array(X_val)[:,:201]
+    X_train, y_train = shuffle(X_train, y_train, random_state=1)
+    # X_val, y_val = shuffle(X_val, y_val, random_state=1)
+    scores = []
+    for n_trees in range(10, 201):
+        forest = RandomForestClassifier(n_estimators=n_trees, random_state=1, n_jobs=10)
+        forest.fit(X_train, y_train)
+        scores.append(forest.score(X_val, y_val))
+    rf_cv_score.append(scores.copy())
+
+
+with open( './rf_cv_score_trajectory_and_start_frame.json', 'w') as f:
+    json.dump(rf_cv_score, f)
+
+
+########################################## Random Forest use only trajectory + start_frame + lifespan_frame ###########################################################################################
+# changing n_estimators in the forest
+rf_cv_score = []
+for i in tqdm(range(6)): # 6-fold cross-validation
+    # train and val dataset
+    embryos_val = embryos_for_cross_validation[4*i:4*i+4]
+    embryos_train = [embryo_name for embryo_name in embryos_for_cross_validation if embryo_name not in embryos_val]
+    X_train, _, y_train = _utilities.prepare_data_for_model(embryo_cells_info, embryos_train, use_frame = True, lifespan_frame_longest = 50, preserve_time_dimension = True, flatten = True)
+    X_val, _, y_val = _utilities.prepare_data_for_model(embryo_cells_info, embryos_val, use_frame = True, lifespan_frame_longest = 50, preserve_time_dimension = True, flatten = True)
+    # use only trajectory features
+    X_train = np.array(X_train)[:,:202] 
+    X_val = np.array(X_val)[:,:202]
+    X_train, y_train = shuffle(X_train, y_train, random_state=1)
+    # X_val, y_val = shuffle(X_val, y_val, random_state=1)
+    scores = []
+    for n_trees in range(10, 201):
+        forest = RandomForestClassifier(n_estimators=n_trees, random_state=1, n_jobs=10)
+        forest.fit(X_train, y_train)
+        scores.append(forest.score(X_val, y_val))
+    rf_cv_score.append(scores.copy())
+
+
+with open( './rf_cv_score_trajectory_and_start_frame_and_lifespan.json', 'w') as f:
+    json.dump(rf_cv_score, f)
+
+
+########################################## Random Forest use only trajectory + start_frame + lifespan_frame + division_orientation_to_mother_cell ###########################################################################################
+# changing n_estimators in the forest
+rf_cv_score = []
+for i in tqdm(range(6)): # 6-fold cross-validation
+    # train and val dataset
+    embryos_val = embryos_for_cross_validation[4*i:4*i+4]
+    embryos_train = [embryo_name for embryo_name in embryos_for_cross_validation if embryo_name not in embryos_val]
+    X_train, _, y_train = _utilities.prepare_data_for_model(embryo_cells_info, embryos_train, use_frame = True, lifespan_frame_longest = 50, preserve_time_dimension = True, flatten = True)
+    X_val, _, y_val = _utilities.prepare_data_for_model(embryo_cells_info, embryos_val, use_frame = True, lifespan_frame_longest = 50, preserve_time_dimension = True, flatten = True)
+    # use only trajectory features
+    X_train = np.array(X_train)[:,:205] 
+    X_val = np.array(X_val)[:,:205]
+    X_train, y_train = shuffle(X_train, y_train, random_state=1)
+    # X_val, y_val = shuffle(X_val, y_val, random_state=1)
+    scores = []
+    for n_trees in range(10, 201):
+        forest = RandomForestClassifier(n_estimators=n_trees, random_state=1, n_jobs=10)
+        forest.fit(X_train, y_train)
+        scores.append(forest.score(X_val, y_val))
+    rf_cv_score.append(scores.copy())
+
+
+with open( './rf_cv_score_trajectory_and_start_frame_and_lifespan_and_division_orientation_to_mother_cell.json', 'w') as f:
+    json.dump(rf_cv_score, f)
